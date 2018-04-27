6f8f9473a05b2b4423fd598ba007836d7cba6476
mohsen <mohsen.alex777@gmail.com>
2018-04-27 18:35:34 +0430

    add project to git

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+         autoPlay:true,
+         items:1,
+         nav:true,
+         center:true
+	});
+});
+$(document).ready(function() {
+        $(".inline-sinup").colorbox({
+            inline: true,
+            width: "450"
+        });
+});
+$(document).ready(function() {
+        $(".inline-login").colorbox({
+            inline: true,
+            width: "450"
+        });
+});
+$(document).ready(function() {
+        $("#cloud").mCustomScrollbar({
+            scrollButtons: {
+                enable: true
+            },
+            theme: "dark-2"
+        });
+});
